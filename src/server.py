e6346ee4afa5e46b97d80ee107fec883848dae46
Kai Anter <kai@anter.dev>
2021-09-22 19:16:59 +0200

    Added: TLS to server

--- src/server.py
+++ src/server.py
@@ -1,9 +1,6 @@
 import socketserver
 import ssl
 
-class ForkingTCPServer(socketserver.ForkingMixIn, socketserver.TCPServer):
-    pass
-
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for our server.
@@ -26,12 +23,27 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                 self.request.sendall(bytes(result, "utf-8"))
             if self.data ==b"end":
                 break
+            if self.data ==b"":
+                break
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
+    PUBLIC_KEY_PATH = "../certs/localhost/cert.pem"
+    PRIVATE_KEY_PATH = "../certs/localhost/key.pem"
 
     # Create the server, binding to localhost on port 9999
-    with ForkingTCPServer((HOST, PORT), MyTCPHandler) as server:
+    # False: bind_and_activate
+    with socketserver.ForkingTCPServer((HOST, PORT), MyTCPHandler, False) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
-        server.serve_forever()
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(PUBLIC_KEY_PATH, PRIVATE_KEY_PATH)
+
+        server.socket = context.wrap_socket(server.socket, server_side=True)
+        server.server_bind()
+        server.server_activate()
+        print("starting server")
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print("\nbye\n")
