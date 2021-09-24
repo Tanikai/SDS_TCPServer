0baacb8901f4d43e0d76a0b55772b38ccce058b7
Kai Anter <kai@anter.dev>
2021-09-24 17:03:54 +0200

    Added: docstrings to server.py

--- src/server.py
+++ src/server.py
@@ -1,20 +1,28 @@
 import socketserver
 import ssl
 
+"""
+This Python3 module contains MyTCPServer, a ForkingTCPServer with TLS support.
+When MyTCPHandler is started, "world" is returned when "hello" is sent over the
+TCP connection.
+The main function contains an example for using MyTCPServer with MyTCPHandler.
+"""
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    Contains the request handler method for the hello world example.
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
+        """
+        Is called for every incoming connection.
+            Input "hello": Output "world"
+            Input "end": connection is closed
+            Input "": connection is closed
+        """
         while True:
-            self.data = self.request.recv(1024).strip()
+            self.data = self.request.recv(1024).strip()  # Retrieve data
             print("{} wrote:".format(self.client_address[0]))
             print(self.data)
 
@@ -29,28 +37,43 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                 print("\nConnection closed\n")
                 break
 
-class MyTCPServer(socketserver.ForkingTCPServer):
-    def __init__(self, server_address, cert, RequestHandlerClass, bind_and_activate=True):
-        super(MyTCPServer, self).__init__(server_address, RequestHandlerClass, False)
-        # socketserver.ForkingTCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate=True)
-        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        self.context.load_cert_chain(cert[0], cert[1])
 
-        self.socket = self.context.wrap_socket(self.socket, server_side=True)
+class MyTCPServer(socketserver.ForkingTCPServer):
+    """
+    TCP server class with TLS. Stop the server with Ctrl+C.
+    """
+
+    def __init__(self, server_address, cert, RequestHandlerClass, bind_and_activate=True):
+        """
+        Constructor. May be extended, do not override.
+
+            Parameters:
+                server_address (str, int): Hostname and Port of the Server
+                cert (str, str): Paths to Public (index 0) and Private (index 1) key for the server.
+                RequestHandlerClass (socketserver.BaseRequestHandler): Handler class for requests.
+                bind_and_activate (bool): When false, server_bind() and server_activate() has to be called later
+        """
+        super(MyTCPServer, self).__init__(
+            server_address, RequestHandlerClass, False)
+
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        # Load Public and Private key into SSL context
+        self.context.load_cert_chain(cert[0], cert[1])
+        self.socket = self.context.wrap_socket(
+            self.socket, server_side=True)  # Add SSL to socket
+
         if bind_and_activate:
             self.server_bind()
             self.server_activate()
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    PUBLIC_KEY_PATH = "../certs/localhost/cert.pem"
-    PRIVATE_KEY_PATH = "../certs/localhost/key.pem"
 
-    # Create the server, binding to localhost on port 9999
-    # False: bind_and_activate
-    with MyTCPServer((HOST, PORT), (PUBLIC_KEY_PATH, PRIVATE_KEY_PATH), MyTCPHandler, True) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
+if __name__ == "__main__":
+    # Example usage of the server
+    HOST, PORT = "localhost", 9999
+    PUB_KEY = "../certs/localhost/cert.pem"
+    PRI_KEY = "../certs/localhost/key.pem"
+
+    with MyTCPServer((HOST, PORT), (PUB_KEY, PRI_KEY), MyTCPHandler) as server:
         print("starting server")
         try:
             server.serve_forever()
